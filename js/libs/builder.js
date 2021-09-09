6196d8fa02ac12674b8143007614a4ef03abed00
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-09-09 10:48:07 +0300

    [ADD] add od-542

--- js/libs/builder.js
+++ js/libs/builder.js
@@ -159,10 +159,38 @@ new Vue({
           allow: 'all',
           type: ['select']
         }
-      ]
+      ],
+      missingColumns: []
     };
   },
+  computed: {
+    hasRequiredFields: function() {
+      return this.fields.some(function(el) {
+        return !!el.required;
+      });
+    },
+    newColumnsNames: {
+      get: function() {
+        return this.missingColumns;
+      },
+      set: function(arr) {
+        this.getNewColumnsName(arr);
+      }
+    },
+    nonExistentColumns: function() {
+      return this.newColumnsNames.join(', ');
+    }
+  },
   methods: {
+    generateColumns: function() {
+      var $vm = this;
+
+      Fliplet.DataSources.update(this.settings.dataSourceId, {
+        newColumns: this.newColumnsNames
+      }).then(function() {
+        $vm.getDataSourceColumns();
+      });
+    },
     onSort: function(event) {
       this.fields.splice(event.newIndex, 0, this.fields.splice(event.oldIndex, 1)[0]);
     },
@@ -294,9 +322,10 @@ new Vue({
       // Cleanup
       this.settings.fields = _.compact(this.fields);
 
-      return Fliplet.Widget.save(this.settings).then(function onSettingsUpdated() {
-        return $vm.updateDataSource();
-      });
+      return Fliplet.Widget.save(this.settings);
+      // return Fliplet.Widget.save(this.settings).then(function onSettingsUpdated() {
+      //   return $vm.updateDataSource();
+      // });
     },
     createDefaultBodyTemplate: function(fields) {
       // Creates default email template
@@ -502,6 +531,20 @@ new Vue({
         this.defaultEmailSettingsForCompose.html = this.createDefaultBodyTemplate(this.fields);
       }
     },
+    getDataSourceColumns: function() {
+      var $vm = this;
+
+      Fliplet.DataSources.getById($vm.settings.dataSourceId, {
+        cache: false,
+        attributes: 'columns'
+      }).then(function(dataSource) {
+        if (!dataSource.columns) {
+          return;
+        }
+
+        $vm.newColumnsNames = dataSource.columns;
+      });
+    },
     updateDataSource: function() {
       var dataSourceId = this.settings.dataSourceId;
       var newColumns = _.chain(this.fields)
@@ -685,6 +728,8 @@ new Vue({
           if (event === 'dataSourceSelect') {
             $vm.settings.dataSourceId = dataSource.id;
           }
+
+          $vm.getDataSourceColumns();
         }
       });
 
@@ -840,6 +885,22 @@ new Vue({
           $vm.useTemplate(blankTemplateId);
         }
       });
+    },
+    getNewColumnsName: function(arr) {
+      var fieldsName = _.chain(this.fields)
+        .filter(function(field) {
+          return field._submit !== false;
+        })
+        .map('name')
+        .value();
+
+      if (!arr.length) {
+        this.missingColumns = fieldsName;
+      }
+
+      this.missingColumns = _.filter(fieldsName, function(item) {
+        return !_.includes(arr, item);
+      });
     }
   },
   watch: {
@@ -964,13 +1025,13 @@ new Vue({
       if (value) {
         this.editor.setContent(this.settings.description);
       }
-    }
-  },
-  computed: {
-    hasRequiredFields: function() {
-      return this.fields.some(function(el) {
-        return !!el.required;
-      });
+    },
+    fields: {
+      deep: true,
+      immediate: true,
+      handler: function() {
+        this.getDataSourceColumns();
+      }
     }
   },
   created: function() {
